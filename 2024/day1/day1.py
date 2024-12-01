813e9ddcbf8201295c810dc95476d3c69235e789
raypatterson77 <ray.patterson@riseup.net>
2024-12-01 15:37:12 +0100

    soltion part2 in python

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,7 +1,16 @@
+def build_dict(l: list) -> dict:
+    sl = set(l)
+    dl = dict.fromkeys(sl, 0)
+    for k, _ in dl.items():
+        dl[k] = l.count(k)
+    return dl
+
 def main():
     left_list = list()
     right_list = list()
-    solution_day1 = 0
+    solution_part1 = 0
+    solution_part2 = 0
+    #prepare input
     with open('input.txt') as f:
         for line in f.readlines():
             line = line.strip()
@@ -12,12 +21,21 @@ def main():
     left_list.sort()
     right_list.sort()
 
+    #part2
+    dict_left = build_dict(left_list)
+    dict_right = build_dict(right_list)
+    for k, v in dict_left.items():
+        solution_part2 = solution_part2 + (k * v * dict_right.get(k, 0))
+
+    #part1
     for x,y in zip(left_list, right_list):
         if (x - y) < 0:
-            solution_day1 = solution_day1 + abs(x-y)
+            solution_part1 = solution_part1 + abs(x-y)
         else:
-            solution_day1 = solution_day1 + (x-y)
-    print(solution_day1)
+            solution_part1 = solution_part1 + (x-y)
+    #output
+    print(f"solution part1 is {solution_part1}")
+    print(f"solution part2 is {solution_part2}")
 
 if __name__ == '__main__':
     main()
